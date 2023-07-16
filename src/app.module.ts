f506a94e00c75f237a04e59d22a06fdfcac4cb9d
Dung Nguyen Van <=>
2023-07-17 01:06:15 +0700

    :wrench: config env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './common/config/app.configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.dev.env',
+      load: [appConfig],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
