95b7339be1ad5c62c0d92bd0781b3b060c0c3025
Nguyễn Văn Dũng <dungtn492@gmail.com>
2023-07-17 01:08:53 +0700

    :wrench: config env vars (#18)
    Co-authored-by: Dung Nguyen Van <=>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = app.get(ConfigService).get('port');
+
+  await app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
 }
 bootstrap();
