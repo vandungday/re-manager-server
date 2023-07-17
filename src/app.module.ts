f7b2bedb8c43514976bb4980a9c80114dce3e958
Dung Nguyen Van <=>
2023-07-17 21:43:11 +0700

    :loud_sound: add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './common/config/app.configuration';
+import { LoggerPinoModule } from './modules/logger/logger.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import appConfig from './common/config/app.configuration';
       envFilePath: process.env.NODE_ENV === 'production' ? '.env' : '.dev.env',
       load: [appConfig],
     }),
+    LoggerPinoModule,
   ],
   controllers: [AppController],
   providers: [AppService],
