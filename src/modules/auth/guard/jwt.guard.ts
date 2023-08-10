cbab2f8a2120c5da1e0adf75ad8d8c3e4df0ba79
Dung Nguyen Van <=>
2023-08-10 23:08:10 +0700

    :+1: implement decorator user

--- src/modules/auth/guard/jwt.guard.ts
+++ src/modules/auth/guard/jwt.guard.ts
@@ -8,12 +8,14 @@ import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
 import { REQUEST_USER_KEY } from '@/common/constants/auth.constant';
+import { PrismaService } from '@/modules/prisma/prisma.service';
 
 @Injectable()
 export class JwtGuard implements CanActivate {
   constructor(
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
+    private readonly prismaService: PrismaService,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -29,7 +31,12 @@ export class JwtGuard implements CanActivate {
         secret: this.configService.get<string>('jwt.secret'),
       });
 
-      request[REQUEST_USER_KEY] = payload;
+      const { userId } = payload;
+      const user = await this.prismaService.user.findUnique({
+        where: { id: userId },
+      });
+
+      request[REQUEST_USER_KEY] = user;
     } catch {
       throw new UnauthorizedException();
     }
